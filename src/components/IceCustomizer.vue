c4091f0ae79ec208f619b0df2b5e41d6d0e35757
AertsMatthew <matthew.aerts@telenet.be>
2025-08-09 23:55:19 +0200

    improve layout for topping selection screen

--- src/components/IceCustomizer.vue
+++ src/components/IceCustomizer.vue
@@ -311,7 +311,7 @@ function resetOrder() {
 
   // ─── 2) **Reset je 3D‑model
   if (iceMesh) {
-    iceMesh.material.color.set('#ffffff')      // terug naar 
+    iceMesh.material.color.set('#ffffff')      
   }
   Object.values(sprinkleMeshes).forEach(m => {
     if (m) m.visible = false                   // alle toppings uit
